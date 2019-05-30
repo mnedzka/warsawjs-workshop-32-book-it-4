634407ead0ffde6c3956185713489c7e9a269914
Michal Nedzka <michael.nedzka@gmail.com>
2019-05-30 18:40:29 +0200

    Add util function for getting value from local storage

--- src/utils/usePersistsValue.js
+++ src/utils/usePersistsValue.js
@@ -1,9 +1,23 @@
 import { useState } from 'react';
 
 export default function usePersistsValue(key, initialValue) {
-  const [storedValue] = useState();
+  const [storedValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
 
-  const setValue = value => {};
+  const setValue = value => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return [storedValue, setValue];
 }
